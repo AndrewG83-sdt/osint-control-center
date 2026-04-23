2bdd3a44b95a9c075122fb43610cbcb7a6664f9d
AndrewG83-sdt <andreagiovannoni1983@hotmail.it>
2026-04-23 09:20:04 +0200

    Update service worker for bootstrap shell

--- sw.js
+++ sw.js
@@ -1,32 +1,25 @@
-const CACHE_NAME = "osint-control-center-v2";
-const ASSETS = [
+const CACHE_NAME = "osint-control-center-v3";
+const CORE_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
+  "./boot.js",
   "./manifest.webmanifest",
   "./favicon.svg",
   "./js/config.js",
   "./js/utils.js",
   "./js/storage.js",
-  "./js/searches.js",
-  "./js/image.js",
-  "./js/graph.js",
-  "./js/exports.js",
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      )
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
     )
   );
 });
@@ -36,15 +29,22 @@ self.addEventListener("fetch", (event) => {
     return;
   }
 
+  const requestUrl = new URL(event.request.url);
+  const isSameOrigin = requestUrl.origin === self.location.origin;
+
+  if (!isSameOrigin) {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) {
-        return cached;
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
       }
 
       return fetch(event.request)
         .then((response) => {
-          if (!response || response.status !== 200 || response.type !== "basic") {
+          if (!response || response.status !== 200) {
             return response;
           }
 
